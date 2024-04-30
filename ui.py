a84e1d419620d07c9c5e12d766b57e24c8c0bfeb
yeeyungong <51457638+yeeyungong@users.noreply.github.com>
2024-04-30 20:01:17 +0800

    Update ui.py

--- ui.py
+++ ui.py
@@ -57,17 +57,10 @@ if st.button("Recommend"):
         num_recommendations = len(recommendations)
         num_rows = (num_recommendations + 2) // 3  # Calculate number of rows needed
         for i in range(num_rows):
-            row_html = "<div style='display:flex;'>"
             for j in range(3):
                 index = i * 3 + j
                 if index < num_recommendations:
                     recommendation = recommendations[index]
-                    # Display the image from GitHub repository using the provided URL
-                    image_url = recommendation['image_url']
-                    # Modify the URL to the correct format
-                    full_image_url = f"{base_github_url}/{image_url}"
-                    # Change the URL to view raw content
-                    full_image_url = full_image_url.replace("/blob/", "/raw/")
                     try:
                         response = requests.get(full_image_url)
                         img = Image.open(BytesIO(response.content))
@@ -84,21 +77,11 @@ if st.button("Recommend"):
                         img = img.resize((250, 250))
                         # Convert the image to base64
                         img_base64 = image_to_base64(img)
-                        # Create HTML for displaying image with title, location, and hashtag
-                        img_html = f"""
-                        <div style="text-align:center;">
-                            <p style="font-weight:bold;">{recommendation['image_title']}</p>
-                            <img src="data:image/jpeg;base64,{img_base64}" style="width:250px; height:250px; margin-bottom:10px;">
-                            <p>Location: {recommendation['location']}</p>
-                            <p>Hashtag: #{recommendation['hashtag']}</p>
-                        </div>
-                        """
-                        row_html += img_html
+                        # Display recommendation information as text
+                        st.text(f"Title: {recommendation['image_title']}\nLocation: {recommendation['location']}\nHashtag: #{recommendation['hashtag']}")
                     except Exception as e:
                         st.write(f"Error loading image from URL: {full_image_url}")
                         st.write(e)
-            row_html += "</div>"
-            st.markdown(row_html, unsafe_allow_html=True)
     else:
         st.write("No recommendations found based on your input.")
 
