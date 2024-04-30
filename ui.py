c3654cd7903d06a68056da3d9c4a0a89f008040f
yeeyungong <51457638+yeeyungong@users.noreply.github.com>
2024-04-30 14:38:12 +0800

    ui.py

--- ui.py
+++ ui.py
@@ -18,7 +18,7 @@ hashtags_str = st.text_input("Enter Hashtags (e.g., cultural tours):")
 
 # Call the recommendation function
 def get_recommendations(location, hashtags_str):
-    travel = pd.read_csv("image_dataset.csv")
+    travel = pd.read_csv("image_dataset.csv") 
 
     # Handle empty hashtags string
     if not hashtags_str:
@@ -40,7 +40,7 @@ def get_recommendations(location, hashtags_str):
             # Get top 10 recommendations
             recommendations = sorted_df[['location', 'hashtag', 'image_url']].head(10).to_dict('records')
             return recommendations
-    return []
+    return [] 
 
 if st.button("Recommend"):
     # Print recommendations if any
@@ -49,8 +49,10 @@ if st.button("Recommend"):
         st.subheader("Recommendations:")
         for recommendation in recommendations:
             st.write(f"- {recommendation['location']}: {recommendation['hashtag']}")
-            # Display the image from GitHub repository
-            st.image(recommendation['image_url'], width=250)
+            # Construct the full path to the image
+            image_path = os.path.join("images", recommendation['image_url'])
+            # Display the image
+            st.image(image_path, width=250)
     else:
         st.write("No recommendations found based on your input.")
 st.stop()
